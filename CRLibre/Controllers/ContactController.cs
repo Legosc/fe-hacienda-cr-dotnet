65f87682620b7e472735eb6277e384a8010b5531
Guillermo Villalobos Davila <gmo_vida@hotmail.com>
2018-09-13 08:11:33 -0600

    no recuerdo que cambios

---
+++ CRLibre/Controllers/ContactController.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRLibre.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+        // GET: Contact
+
+        public ActionResult Index()
+        {
+            var claims = User.Identity as ClaimsIdentity;
+            
+            var response = client.GetAsync(string.Format("http://localhost:3000/clients?userName={0}&sessionKey={1}", User.Identity.Name, claims.FindFirst(ClaimTypes.Sid).Value)).Result;
+            var responseString =  response.Content.ReadAsStringAsync().Result;
+            JObject jObject = JObject.Parse("{ \"Contacts\" : " + responseString + "}");
+            JToken jResult = jObject["Contacts"];
+
+            return View();
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contact/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRLibre/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRLibre.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int tipo_identificacion { get; set; }
+        public int numero_identificacion { get; set; }
+    }
+}
